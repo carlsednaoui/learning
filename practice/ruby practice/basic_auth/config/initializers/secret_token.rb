ac6d6043f5c91b5fffefc1022e5572d2b23f6025
carl sednaoui <carlsednaoui@gmail.com>
2013-06-25 15:17:56 -0400

    Ruby practice: basic oauth

---
+++ practice/ruby practice/basic_auth/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BasicAuth::Application.config.secret_token = 'dc34615590d95a79b69e1e2e059e50c8793968b67759835171f34d4b38fefbe256299af48a8cb72ec0efe3879fe2471bc169f8138218ee67d66a74439c96eef6'
